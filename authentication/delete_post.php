a3f52008ca631147232a82f684d4c7ec657f817e
fog <kuhj100311@gmail.com>
2026-07-07 22:45:55 -0400

    added prepared statements to deleting posts option

--- authentication/delete_post.php
+++ authentication/delete_post.php
@@ -1,11 +1,16 @@
 <?php
+ini_set('display_errors',1);
 require '../authentication/db.php';
 require '../authentication/session_check.php';
 session_check();
 $id = $_GET['id'];
+##
+$delete_post_query = "delete from post where id = ?"; #trying '?' will treat ? as text, not parameter bind.
+$stmt = $db_conn_prepared->prepare($delete_post_query);
+$stmt->bind_param("s",$id);
+$stmt->execute();
 
-$delete_post_query = "delete from post where id = '$id'";
-mysqli_query($db_conn,$delete_post_query);
+#mysqli_query($db_conn,$delete_post_query);
 
 echo "<script> alert('Delete Complete.');</script>";
 echo "<script>location.href='../protected/post.php';</script>";
